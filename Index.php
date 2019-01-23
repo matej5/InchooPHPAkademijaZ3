61d3faea1b34c0dcbe5bef266fb2b12d737dad6b
matej5 <30931414+matej5@users.noreply.github.com>
2019-01-23 17:45:47 +0100

    function created

---
+++ Index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include_once 'function.php';
+
+$a = 7;
+$b = 4;
+$arr = spiralFill($a, $b);
+?>
+<table id="table">
+    <?php
+    for ($y = 0; $y < $a; $y++) {
+        echo '<tr>';
+        for ($x = 0; $x < $b; $x++) {
+            if ($x === 0 && $y === 0) {
+                echo '<td id="cellColor">', ($arr[$y][$x]), '</td>';
+            } else {
+                echo '<td id="cell">', ($arr[$y][$x]), '</td>';
+            }
+        }
+        echo '</tr>';
+    }
+    ?>
+</table>
+</body>
+</html>
---
+++ function.php
@@ -0,0 +1,34 @@
+<?php
+function spiralFill($m, $n)
+{
+    $val = 1;
+    $arr = [];
+    $k = 0;
+    $l = 0;
+    while ($k < $m && $l < $n)
+    {
+        for ($i = $l; $i < $n; ++$i)
+            $arr[$k][$i] = $val++;
+
+        $k++;
+
+        for ($i = $k; $i < $m; ++$i)
+            $arr[$i][$n - 1] = $val++;
+        $n--;
+
+        if ($k < $m)
+        {
+            for ($i = $n - 1; $i >= $l; --$i)
+                $arr[$m - 1][$i] = $val++;
+            $m--;
+        }
+
+        if ($l < $n)
+        {
+            for ($i = $m - 1; $i >= $k; --$i)
+                $arr[$i][$l] = $val++;
+            $l++;
+        }
+    }
+    return $arr;
+}
